00095fd741c1b1ece0f2b47bd8f4457296f716f0
monkm <monkm@192.168.0.29>
2022-03-11 15:46:49 +0000

    Added Encoding and

--- JsonFileMerger/src/jsonFileMerger/JsonFileMergeExecute.java
+++ JsonFileMerger/src/jsonFileMerger/JsonFileMergeExecute.java
@@ -1,11 +1,20 @@
 package jsonFileMerger;
 
+import java.io.IOException;
+
 public class JsonFileMergeExecute {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IllegalArgumentException, IOException {
 		String InputFolder = args[0];
 		String OutputFolder = args[1];
 		String SoftType = args[2];
+		String JSONEncoding = args[3];
+		JsonFileMerger jsonFileMerger = new JsonFileMerger();
+		jsonFileMerger.setArrayField(SoftType);
+		jsonFileMerger.setInputFolder(InputFolder);
+		jsonFileMerger.setJSONEncoding(JSONEncoding);
+		jsonFileMerger.setOutputFolder(OutputFolder);
+		jsonFileMerger.execute();
 	}
 
 }
--- JsonFileMerger/src/jsonFileMerger/JsonFileMerger.java
+++ JsonFileMerger/src/jsonFileMerger/JsonFileMerger.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import com.fasterxml.jackson.core.JsonEncoding;
@@ -19,39 +18,75 @@ public class JsonFileMerger {
 
 	private JsonFactory jfactory = new JsonFactory();
 	private String OutputFolder;
-	private String SoftType;
+	private String ArrayField;
 	private String InputFolder;
-	private ObjectMapper mapper = new ObjectMapper();
-	public void execute() throws IOException {
+	private JsonEncoding JSONEncoding = JsonEncoding.UTF8;
+	private ObjectMapper Mapper = new ObjectMapper();
+	
 
-
-		FileOutputStream fos = new FileOutputStream(new File(OutputFolder + SoftType + ".json"));
-
-		JsonGenerator jGenerator = jfactory.createGenerator(fos, JsonEncoding.UTF8);
+	public void execute() throws IOException, IllegalArgumentException  {
+		validateConstructor();
+		FileOutputStream fileOut = new FileOutputStream(new File(OutputFolder +"/"+ ArrayField + ".json"));
+		JsonGenerator jGenerator = jfactory.createGenerator(fileOut, JSONEncoding);
 		jGenerator.writeStartObject();
-		jGenerator.writeFieldName(SoftType);
+		jGenerator.writeFieldName(ArrayField);
 		jGenerator.writeStartArray();
 		Files.walk(Paths.get(InputFolder)).filter(p -> p.toString().endsWith(".json")).forEach(p -> {
 			try {
-
 				InputStream jsonStream = new FileInputStream(p.toFile());
 				JsonParser jsonParser = new JsonFactory().createParser(jsonStream);
-				TreeNode treeNode = mapper.readTree(jsonParser);
-				jGenerator.setCodec(mapper);
+				TreeNode treeNode = Mapper.readTree(jsonParser);
+				jGenerator.setCodec(Mapper);
 				jGenerator.writeTree(treeNode);
 				jsonStream.close();
 				jsonParser.close();
-
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		});
-
 		jGenerator.writeEndArray();
-
 		jGenerator.close();
-		fos.close();
+		fileOut.close();
 
 	}
+
+	public void setOutputFolder(String outputFolder) {
+		OutputFolder = outputFolder;
+	}
+
+	public void setArrayField(String arrayField) {
+		ArrayField = arrayField;
+	}
+
+	public void setInputFolder(String inputFolder) {
+		InputFolder = inputFolder;
+	}
+
+	public void setJSONEncoding(String jSONEncoding) {
+
+		if (jSONEncoding == "UTF8") {
+			JSONEncoding = JsonEncoding.UTF8;
+		} else if (jSONEncoding == "UTF16_BE") {
+			JSONEncoding = JsonEncoding.UTF16_BE;
+		} else if (jSONEncoding == "UTF16_LE") {
+			JSONEncoding = JsonEncoding.UTF16_LE;
+		} else if (jSONEncoding == "UTF32_BE") {
+			JSONEncoding = JsonEncoding.UTF32_BE;
+		} else if (jSONEncoding == "UTF32_LE") {
+			JSONEncoding = JsonEncoding.UTF32_LE;
+		}
+	}
+	
+    private void validateConstructor() {
+        if(InputFolder == null ) {
+            throw new IllegalArgumentException("InputFolder");
+        }
+        if(ArrayField == null ) {
+            throw new IllegalArgumentException("ArrayField");
+        }
+        if(OutputFolder == null ) {
+            throw new IllegalArgumentException("OutputFolder");
+        }
+    }
 
 }
